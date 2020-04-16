ec1ae69f6facfce55907994f2e5892a585d6d9be
sdiamondj <954935930@qq.com>
2020-04-16 14:43:18 +0800

    商品搜索功能完成

---
+++ src/main/java/cn/edu/ncu/trading_server/mapper/GoodMapper.java
@@ -0,0 +1,31 @@
+package cn.edu.ncu.trading_server.mapper;
+
+import cn.edu.ncu.trading_server.entity.Good;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface GoodMapper {
+    int deleteByPrimaryKey(Integer goodsId);
+
+    int insert(Good record);
+
+    int insertSelective(Good record);
+
+    Good selectByPrimaryKey(Integer goodsId);
+
+    int updateByPrimaryKeySelective(Good record);
+
+    int updateByPrimaryKey(Good record);
+
+    List<Good> selectAll();
+
+    List<Good> selectByGame(int gameId);
+
+    List<Good> selectByTarget(String target);
+
+    List<Good> selectAllCanBuy();
+}
